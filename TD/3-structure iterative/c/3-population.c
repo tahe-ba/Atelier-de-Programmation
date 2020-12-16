d798f5ad494ac2c2163e7a7d0ba7b819fdc283f7
taha <taha.ben.a@outlook.com>
2020-12-16 07:47:33 +0100

    4-ex3

--- TD/3-structure iterative/c/3-population.c
+++ TD/3-structure iterative/c/3-population.c
@@ -1,15 +1,17 @@
-//clear
-/*
 #include<stdio.h>
-int main()
-{
-int n=-3;
-n++;
-if (n > -3) {
-printf("n=%d\n", --n);
-} else {
-printf("n-2=%d\n", n-2);
+
+void main() {
+  double p;
+  int annee, i;
+  p = 9380404;
+  //au debut 1999
+  p = p + p * (0.2007 + 0.0506- 0.0073) * 0.5;
+  do {
+  printf("entrer l annee:");
+  scanf("%d", & annee);
+  }while (annee<1999)  ;
+  for (i = 2000; i <= annee; i++) {
+    p = p + p *(0.2007 + 0.0506- 0.0073);
+  }
+  printf("La population debut %d est %.0f\n", annee, p);
 }
-return 0;
-}
-/*
