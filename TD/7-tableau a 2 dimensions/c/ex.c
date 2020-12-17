579c03bc8b6f71b4921276bcf28fc2f65828b76e
taha <taha.ben.a@outlook.com>
2020-12-17 09:36:29 +0100

    test

--- TD/7-tableau a 2 dimensions/c/ex.c
+++ TD/7-tableau a 2 dimensions/c/ex.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 void main(){
     int x,y,z,t=1;
-    do{
+   // do{
         printf("x=");
         scanf("%d",&x);
-    }while ((x<0)||(x>99));
-    do{
+   // }while ((x<0)||(x>99));
+   // do{
         printf("y=");
         scanf("%d",&y);
-    }while ((y<0)||(y>99));
+    //}while ((y<0)||(y>99));
    while (t <= y){
     t =t* 10;
     printf("%d\n",t);
