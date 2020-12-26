c6b3535bcafb500af01915467d0bff70ac4d9011
taha <taha.ben.a@outlook.com>
2020-12-26 13:31:14 +0100

    symetrie

---
+++ TD/7-tableau a 2 dimensions/c/4-symetrie.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+void input(int* n) {
+  do {
+    printf(">>");
+    scanf("%d", n);
+  } while (*n <= 0);
+}
+//t+i = &t[i]
+
+void remplir(int (*t)[50], int n) {
+    int i,j;
+    printf("Remplissage du tableau de taille [%d][%d]\n",n,n);
+   for (i=0; i<n; i++)
+    for (j=0; j<n; j++)
+        {
+         printf("Elément[%d][%d] : ",i,j);
+         scanf("%d", &t[i][j]);
+        }
+}
+
+void afficher(int (*t)[50], int n) {
+    int i,j;
+    for (i=0;i<n;i++){
+    printf(">>>>>>>>");
+    }
+    printf("\n");
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            printf("%d\t",t[i][j]);
+        }
+        printf("\n");
+    }
+    for (i=0;i<n;i++){
+    printf(">>>>>>>>");
+    }
+    printf("\n");
+} 
+
+int isSYM(int (*t)[50], int n){
+    int a=1,i,j;
+    for(i=0;i<n;i++){
+       for(j=0;j<n;j++){
+           if ( t[i][j] != t [j][i])
+               return 0;
+       }
+    }
+    return 1;
+}
+
+void main (){
+    int n,i=0,j,a;
+    int t[50][50];
+    printf("ordre de matrice :");
+    input(&n);
+    remplir(t,n);
+    afficher(t,n);
+if (isSYM(t,n)==1) {
+    printf("vrai\n");
+}else if (isSYM(t,n)==0){
+    printf("faux\n");
+}
+}
